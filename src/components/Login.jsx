648b46e502935e06df656bb0bf41c60e6213e198
ErickMelendez25 <a20213712@pucp.edu.pe>
2025-03-18 13:10:04 -0500

    V0.0004 DE SATELITERRENO

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const navigate = useNavigate();
 
   const apiUrl = process.env.NODE_ENV === 'production'
-    ? 'https://gestioncalidaduncp-production.up.railway.app'
+    ? 'https://sateliterreno-production.up.railway.app'
     : 'http://localhost:5000';
 
   const handleLogin = async (e) => {
